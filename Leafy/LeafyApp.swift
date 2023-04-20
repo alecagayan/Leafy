c617d2a6ad500800e1dd62f3082d9b1bfaa04311
Alec <alecagayan24@gmail.com>
2023-04-20 12:00:05 -0400

    Initial Commit

---
+++ Leafy/LeafyApp.swift
@@ -0,0 +1,17 @@
+//
+//  LeafyApp.swift
+//  Leafy
+//
+//  Created by Alec Agayan on 4/20/23.
+//
+
+import SwiftUI
+
+@main
+struct LeafyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
